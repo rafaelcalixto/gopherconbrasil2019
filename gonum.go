cbee2b5558511af6739ae97365cb860cedfeea5b
rafaelcalixto <rafaelufrj@yahoo.com.br>
2019-09-16 15:07:14 -0300

    updating...

--- gonum.go
+++ gonum.go
@@ -1,50 +1,25 @@
 package main
 
 import (
-    "net/http"
+    gcbr "gopherconbr2019"
     "fmt"
-    "log"
-    "context"
-    "time"
-    "io/ioutil"
-    "strings"
 )
 
 var (
-    url        string
-    kv_str     string
-    key_values []string
+    mun_url    string
+    stats_url  string
     sc_schools map[string]string
-    key        string
-    value      string
+    stats_sc   map[string]string
+    infoLab    float64
 )
 func main() {
-    ctx, cancel := context.WithTimeout(context.Background(), time.Second * 30)
-    defer cancel()
+    mun_url = "http://educacao.dadosabertosbr.com/api/cidades/SC"
+    stats_url = "http://educacao.dadosabertosbr.com/api/estatisticas?codMunicipio="
 
-    url = "http://educacao.dadosabertosbr.com/api/cidades/SC"
-    ans, err := http.NewRequest("GET", url, nil)
-    if err != nil { log.Fatal("Request fail: %v", err) }
+    sc_schools = gcbr.GetMunicipios(gcbr.GetData(mun_url))
 
-    ans = ans.WithContext(ctx)
-    resp, _ := http.DefaultClient.Do(ans)
-    defer resp.Body.Close()
-
-    bytes, err := ioutil.ReadAll(resp.Body)
-    sc_schools = make(map[string]string)
-
-    for _, kv_str := range strings.Split(string(bytes), ",") {
-        key_values = strings.Split(kv_str, ":")
-        key = strings.Replace(key_values[0], `"`, "", -1)
-        key = strings.Replace(key, "[", "", -1)
-        value = strings.Replace(key_values[1], `"`, "", -1)
-        value = strings.Replace(value, "[", "", -1)
-
-        sc_schools[key] = value
+    for k, _ := range sc_schools {
+        stats_sc = gcbr.GetStats(gcbr.GetData(stats_url + k), "laboratorioInformatica")
+        fmt.Println(infoLab)
     }
-
-    for _, v := range sc_schools {
-        fmt.Println(v)
-    }
-
 }
